9fd7937c8c576c394f4da1fa3194fefc20099997
rotto5 <ichigokani2@gmail.com>
2025-08-12 12:00:00 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($onion_data);
+
+    ?>
+  </p>
+</body>
+
+</html>
